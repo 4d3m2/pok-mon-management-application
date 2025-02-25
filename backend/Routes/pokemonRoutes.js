11829b5febaf3cee7d5a23e1593237be2372c420
4d3m2 <148943293+4d3m2@users.noreply.github.com>
2025-02-25 09:33:35 +0100

    Update pokemonRoutes.js

--- backend/Routes/pokemonRoutes.js
+++ backend/Routes/pokemonRoutes.js
@@ -13,13 +13,15 @@ const {
 const token = require('../auth/middleware');
 const pokemonRouter = express.Router();
 
-pokemonRouter.get('/:id', getPokemonById);  
+
 
 
 pokemonRouter.get('/filter', token, filterPokemonByType);
 pokemonRouter.get('/sort', token, getPokemonSortedByDate);
 pokemonRouter.get('/search', token, searchPokemonByName);  
 pokemonRouter.get('/types', token, getPokemonTypes);
+
+pokemonRouter.get('/:id', getPokemonById);  
 
 pokemonRouter.get('/', token, getAllPokemon);
 pokemonRouter.post('/', token, addPokemon);
